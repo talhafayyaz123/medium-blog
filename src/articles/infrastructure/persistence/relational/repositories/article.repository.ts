bf909df0d933e64a6b630f8db09c39a5807f4f88
Hassaan Sadiq <hhsadiq@gmail.com>
2024-08-02 14:52:30 +0500

    chore(article): renamed the article createdAt to created_at

--- src/articles/infrastructure/persistence/relational/repositories/article.repository.ts
+++ src/articles/infrastructure/persistence/relational/repositories/article.repository.ts
@@ -7,7 +7,6 @@ import { Article } from '../../../../domain/article';
 import { ArticleRepository } from '../../article.repository';
 import { ArticleMapper } from '../mappers/article.mapper';
 import { IPaginationOptions } from '../../../../../utils/types/pagination-options';
-import { UserEntity } from '../../../../../users/infrastructure/persistence/relational/entities/user.entity';
 
 @Injectable()
 export class ArticleRelationalRepository implements ArticleRepository {
