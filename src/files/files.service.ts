3f941007e4b5d1771c8b0d7183934a5cd8abc150
Ahmad Bilal <ahmadbilal.3491@gmail.com>
2024-09-06 12:18:35 +0500

    fix(file): update class name in file module

--- src/files/files.service.ts
+++ src/files/files.service.ts
@@ -3,11 +3,11 @@ import { Injectable } from '@nestjs/common';
 import { NullableType } from '@src/utils/types/nullable.type';
 
 import { FileType } from './domain/file';
-import { FileRepository } from './infrastructure/persistence/file.repository';
+import { FileAbstractRepository } from './infrastructure/persistence/file.abstract.repository';
 
 @Injectable()
 export class FilesService {
-  constructor(private readonly fileRepository: FileRepository) {}
+  constructor(private readonly fileRepository: FileAbstractRepository) {}
 
   findById(id: FileType['id']): Promise<NullableType<FileType>> {
     return this.fileRepository.findById(id);
--- src/files/infrastructure/uploader/local/files.service.ts
+++ src/files/infrastructure/uploader/local/files.service.ts
@@ -5,13 +5,13 @@ import { ERROR_MESSAGES } from '@src/common/constants';
 import { UNPROCESSABLE_ENTITY } from '@src/common/exceptions';
 import { AllConfigType } from '@src/config/config.type';
 import { FileType } from '@src/files/domain/file';
-import { FileRepository } from '@src/files/infrastructure/persistence/file.repository';
+import { FileAbstractRepository } from '@src/files/infrastructure/persistence/file.abstract.repository';
 
 @Injectable()
 export class FilesLocalService {
   constructor(
     private readonly configService: ConfigService<AllConfigType>,
-    private readonly fileRepository: FileRepository,
+    private readonly fileRepository: FileAbstractRepository,
   ) {}
 
   async create(file: Express.Multer.File): Promise<{ file: FileType }> {
